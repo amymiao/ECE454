5415c40a20ad7fbf86fd3d49a52bfd43dd3b7829
evfrancis <ev.louis.francis@gmail.com>
2013-11-07 00:48:41 -0500

    Adding code for explicit free list, has bugs

--- hw3/assn/mm.c
+++ hw3/assn/mm.c
@@ -32,13 +32,13 @@ team_t team = {
     /* Second member's full name (leave blank if none) */
     "Everard Francis",
     /* Second member's email address (leave blank if none) */
-    "everard.francis@utoronto.ca"
+    "everard.francis@mail.utoronto.ca"
 };
 
 /*************************************************************************
  * Basic Constants and Macros
  * You are not required to use these macros but may find them helpful.
-*************************************************************************/
+ *************************************************************************/
 #define WSIZE       sizeof(void *)         /* word size (bytes) */
 #define DSIZE       (2 * WSIZE)            /* doubleword size (bytes) */
 #define CHUNKSIZE   (1<<7)      /* initial heap size (bytes) */
@@ -70,25 +70,34 @@ team_t team = {
 /* rounds up to the nearest multiple of ALIGNMENT */
 #define ALIGN(size) (((size) + (ALIGNMENT-1)) & ~0xf)
 
+int mm_check();
+
 void* heap_listp = NULL;
+
+typedef struct free_block {
+    struct free_block* next;
+    struct free_block* prev;
+} free_block;
+
+free_block* free_list;
 
 /**********************************************************
  * mm_init
  * Initialize the heap, including "allocation" of the
  * prologue and epilogue
  **********************************************************/
- int mm_init(void)
- {
-   if ((heap_listp = mem_sbrk(4*WSIZE)) == (void *)-1)
-         return -1;
-     PUT(heap_listp, 0);                         // alignment padding
-     PUT(heap_listp + (1 * WSIZE), PACK(OVERHEAD, 1));   // prologue header
-     PUT(heap_listp + (2 * WSIZE), PACK(OVERHEAD, 1));   // prologue footer
-     PUT(heap_listp + (3 * WSIZE), PACK(0, 1));    // epilogue header
-     heap_listp += DSIZE;
+int mm_init(void)
+{
+    if ((heap_listp = mem_sbrk(4*WSIZE)) == (void *)-1)
+        return -1;
+    PUT(heap_listp, 0);                         // alignment padding
+    PUT(heap_listp + (1 * WSIZE), PACK(OVERHEAD, 1));   // prologue header
+    PUT(heap_listp + (2 * WSIZE), PACK(OVERHEAD, 1));   // prologue footer
+    PUT(heap_listp + (3 * WSIZE), PACK(0, 1));    // epilogue header
+    heap_listp += DSIZE;
 
-     return 0;
- }
+    return 0;
+}
 
 /**********************************************************
  * coalesce
@@ -165,14 +174,65 @@ void *extend_heap(size_t words)
  **********************************************************/
 void * find_fit(size_t asize)
 {
-    void *bp;
-    for (bp = heap_listp; GET_SIZE(HDRP(bp)) > 0; bp = NEXT_BLKP(bp))
-    {
-        if (!GET_ALLOC(HDRP(bp)) && (asize <= GET_SIZE(HDRP(bp))))
-        {
-            return bp;
-        }
+    // Traverse the free list and find the first fitting block
+    // If there is excess space in the free block we found, we
+    // place the remainder back into the free list as a smaller free block
+    if (free_list == NULL) {
+        // No free blocks in the free list
+        return NULL;
     }
+    free_block* temp = free_list;
+    do {
+        int size = GET_SIZE(HDRP(temp));
+        if (size >= asize && size < (asize + 2*DSIZE)) {
+            // We found a block that can fit, but cannot be split into an excess free block
+            // This is easy to resolve, simply remove it from the free list
+            if (temp != temp->next) {
+                temp->prev->next = temp->next; // Make previous free block point to next free block
+                temp->next->prev = temp->prev; // Make next free block point to previous free block
+                if (free_list == temp) {
+                    // If we are removing the head pointer, we must set a new head pointer
+                    free_list = temp->next;
+                }
+            } else {
+                free_list = NULL;
+            }
+            return (void*)temp;
+        } else if (size >= (asize + 2*DSIZE)) {
+            // The block found has excess size and we can split the excess
+            // into a free block
+            // Example: Requested size = 4
+            //          Found free block size = 10
+            // [H1][ ][ ][ ][ ][ ][ ][ ][ ][F1]
+            // newSize = 6
+            int newSize = size - asize;
+
+            // Point to the ending portion of the free block, so that
+            // we can assign this portion to the user
+            void* userPtr = (void*)temp + newSize;
+
+            // Set the header/footer of the user required block with their requested size 
+            // Example (cont): [H1][ ][ ][ ][ ][ ][H2][ ][ ][F2]
+            PUT(HDRP(userPtr), PACK(asize,0));
+            PUT(FTRP(userPtr), PACK(asize,0));
+
+            // Adjust the size of the free block
+            // Example (cont): [H1][ ][ ][ ][ ][F1][H2][ ][ ][F2]
+            PUT(HDRP(temp), PACK(newSize,0));
+            PUT(FTRP(temp), PACK(newSize,0));
+            return userPtr;
+        } 
+        temp = temp->next;
+    } while (temp != free_list);
+    /*void *bp;
+
+      for (bp = heap_listp; GET_SIZE(HDRP(bp)) > 0; bp = NEXT_BLKP(bp))
+      {
+      if (!GET_ALLOC(HDRP(bp)) && (asize <= GET_SIZE(HDRP(bp))))
+      {
+      return bp;
+      }
+      }*/
     return NULL;
 }
 
@@ -182,26 +242,52 @@ void * find_fit(size_t asize)
  **********************************************************/
 void place(void* bp, size_t asize)
 {
-  /* Get the current block size */
-  size_t bsize = GET_SIZE(HDRP(bp));
+    /* Get the current block size */
+    size_t bsize = GET_SIZE(HDRP(bp));
 
-  PUT(HDRP(bp), PACK(bsize, 1));
-  PUT(FTRP(bp), PACK(bsize, 1));
+    PUT(HDRP(bp), PACK(bsize, 1));
+    PUT(FTRP(bp), PACK(bsize, 1));
 }
 
 /**********************************************************
  * mm_free
  * Free the block and coalesce with neighbouring blocks
+ * This function frees a block and inserts it into the free_list
+ * There is no additional overhead, as the space we freed up is used for the entry and is
+ * invisible to the user.
  **********************************************************/
 void mm_free(void *bp)
 {
     if(bp == NULL){
-      return;
+        return;
     }
     size_t size = GET_SIZE(HDRP(bp));
+    
     PUT(HDRP(bp), PACK(size,0));
     PUT(FTRP(bp), PACK(size,0));
-    coalesce(bp);
+    printf("RECEIVED FREE (0x%x), size=%d\n",bp,size);
+
+    free_block* temp = (free_block*)bp;
+
+    if (free_list == NULL) {
+        // The free list is empty, this will be the first free block. Set head to point to it.
+        free_list = temp;
+        free_list->next = temp;
+        free_list->prev = temp;
+    } else {
+        // Insert the newly freed block into the start of the free list
+        // It will become the new head
+        temp->next = free_list;
+        temp->prev = free_list->prev;
+        temp->prev->next = temp;
+        temp->next->prev = temp;
+    }
+
+    
+    mm_check();
+
+    // FIXME: Uncomment
+    //coalesce(bp);
 }
 
 
@@ -215,6 +301,7 @@ void mm_free(void *bp)
  **********************************************************/
 void *mm_malloc(size_t size)
 {
+    
     size_t asize; /* adjusted block size */
     size_t extendsize; /* amount to extend heap if no fit */
     char * bp;
@@ -230,8 +317,11 @@ void *mm_malloc(size_t size)
         asize = DSIZE * ((size + (OVERHEAD) + (DSIZE-1))/ DSIZE);
 
     /* Search the free list for a fit */
+    
     if ((bp = find_fit(asize)) != NULL) {
         place(bp, asize);
+        printf("SERVICED MALLOC (0x%x), size=%d\n",bp,asize);
+        mm_check();
         return bp;
     }
 
@@ -240,6 +330,9 @@ void *mm_malloc(size_t size)
     if ((bp = extend_heap(extendsize/WSIZE)) == NULL)
         return NULL;
     place(bp, asize);
+    printf("SERVICED MALLOC (0x%x), size=%d\n",bp,asize);
+    mm_check();
+    
     return bp;
 
 }
@@ -252,13 +345,13 @@ void *mm_realloc(void *ptr, size_t size)
 {
     /* If size == 0 then this is just free, and we return NULL. */
     if (size == 0){
-      mm_free(ptr);
-      return NULL;
+        mm_free(ptr);
+        return NULL;
     }
 
     /* If old ptr is NULL, then this is just malloc. */
     if (ptr == NULL)
-      return (mm_malloc(size));
+        return (mm_malloc(size));
 
     void *oldptr = ptr;
     void *newptr;
@@ -266,12 +359,12 @@ void *mm_realloc(void *ptr, size_t size)
 
     newptr = mm_malloc(size);
     if (newptr == NULL)
-      return NULL;
+        return NULL;
 
     /* Copy the old data. */
     copySize = GET_SIZE(HDRP(oldptr));
     if (size < copySize)
-      copySize = size;
+        copySize = size;
     memcpy(newptr, oldptr, copySize);
     mm_free(oldptr);
     return newptr;
@@ -283,5 +376,13 @@ void *mm_realloc(void *ptr, size_t size)
  * Return nonzero if the heap is consistant.
  *********************************************************/
 int mm_check(void){
-  return 1;
+    void* start = heap_listp;
+
+    printf("\n\nHEAP STATS:\n");
+    while (GET_SIZE(HDRP(start)) != 0) {
+        printf("Address: 0x%x\tSize: %d\tAllocated: %d\n",start,GET_SIZE(HDRP(start)),GET_ALLOC(HDRP(start)));
+        start = NEXT_BLKP(start); 
+    }
+    printf("\n");
+    return 1;
 }
