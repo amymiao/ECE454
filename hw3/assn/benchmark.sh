73eca1dfc5c65cfca3494104b7a66ff9c23c0f1e
evfrancis <ev.louis.francis@gmail.com>
2013-11-08 20:11:32 -0500

    adding benchmark

---
+++ hw3/assn/benchmark.sh
@@ -0,0 +1,22 @@
+echo "amptjp-bal.rep"
+./mdriver -v -f ../traces/amptjp-bal.rep
+echo "binary2-bal.rep"
+./mdriver -v -f ../traces/binary2-bal.rep
+echo "binary-bal.rep"
+./mdriver -v -f ../traces/binary-bal.rep
+echo "cccp-bal.rep"
+./mdriver -v -f ../traces/cccp-bal.rep
+echo "coalescing-bal.rep"
+./mdriver -v -f ../traces/coalescing-bal.rep
+echo "cp-decl-bal.rep"
+./mdriver -v -f ../traces/cp-decl-bal.rep
+echo "expr-bal.rep"
+./mdriver -v -f ../traces/expr-bal.rep
+echo "random2-bal.rep"
+./mdriver -v -f ../traces/random2-bal.rep
+echo "random-bal.rep"
+./mdriver -v -f ../traces/random-bal.rep
+echo "realloc2-bal.rep"
+./mdriver -v -f ../traces/realloc2-bal.rep
+echo "realloc-bal.rep"
+./mdriver -v -f ../traces/realloc-bal.rep
