78a155eddebacf73a69d591e83960b06efe72fe6
evfrancis <everard.francis@utoronto.ca>
2013-12-05 20:14:24 -0500

    Comments, flipping loops for cache performance

--- hw5/life.c
+++ hw5/life.c
@@ -40,17 +40,22 @@ threaded_game_of_life (void * inputs)
     const int end_index = input_args->end_index;
     const int gens_max = input_args->gens_max;
     
+    /* Each thread will work on a horizontal segment of the board
+     * This segment is defined by the rows start_index and end_index
+     * At the end of a generation, a thread must wait for all other threads to complete
+     * We use a barrier to do this. This makes sure that all threads read the same board data
+     */
+    // OPTIMIZATION NOTE: Flip the loops for caches. Traverse horizontally on each row.
     for(curgen=0; curgen<gens_max; curgen++) 
     {
-        for(i=start_index; i<=end_index; i++) 
+        for(j=0; j<ncols; j++)
         {
-            const int inorth = mod (i-1, nrows);
-            const int isouth = mod (i+1, nrows);
-            for(j=0; j<ncols; j++)
+            const int jwest = mod (j-1, ncols);
+            const int jeast = mod (j+1, ncols);
+            for(i=start_index; i<=end_index; i++) 
             {
-                const int jwest = mod (j-1, ncols);
-                const int jeast = mod (j+1, ncols);
-                
+                const int inorth = mod (i-1, nrows);
+                const int isouth = mod (i+1, nrows);
                 const char neighbor_count = 
                     BOARD (input_args->inboard, inorth, jwest) + 
                     BOARD (input_args->inboard, inorth, j) + 
@@ -64,6 +69,9 @@ threaded_game_of_life (void * inputs)
                 BOARD(input_args->outboard, i, j) = alivep(neighbor_count, BOARD (input_args->inboard, i, j));
             }
         }
+
+        // Wait till all threads are done before switching the boards and continuing,
+        // for consistency
         pthread_barrier_wait(input_args->barr);
         SWAP_BOARDS(input_args->outboard, input_args->inboard);
     }
@@ -85,14 +93,17 @@ game_of_life (char* outboard,
   const int LDA = nrows;
   
   //lower than the minimum size - do this in case someone runs N < 32
-  if (nrows > 32)
+  if (nrows < 32)
     return sequential_game_of_life (outboard, inboard, nrows, ncols, gens_max);
   
   //Setup pthread and synchronization primitives
   pthread_t tid[NUM_THREADS];
   pthread_barrier_t barr;
   
-  //Setup thread argument structure
+  /* Setup thread argument structure
+   * We divide the board into NUM_THREADS horizontal slices
+   * Each thread gets one slice to work on
+   */
   thread_input t_input[NUM_THREADS];
   int i;
   for(i=0;i<NUM_THREADS;i++)
